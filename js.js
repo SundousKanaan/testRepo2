4b113c1ab3e8ecbfa1e656a445d95b80678c31aa
WaleedSalahEldeen <waleed9salah@gmail.com>
2024-04-11 23:49:48 +0200

    initial commit

--- js.js
+++ js.js
@@ -2,7 +2,6 @@
 // Zorg ervoor dat dit pad correct is naar je background-check.js-bestand
 // Als het zich in dezelfde map bevindt als je script.js, laat het dan zoals het is
 // Anders moet je het juiste pad specificeren
-import BackgroundCheck from "/background-check";
 
 // Wacht tot het HTML-document volledig is geladen voordat je de code uitvoert
 document.addEventListener("DOMContentLoaded", function () {
@@ -13,4 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // Debug-modus inschakelen om uitvoer in de console te tonen
     debug: true,
   });
+
+
+    const img = document.getElementById('draggableImage');
+    const thumbnail = document.querySelector('.thumbnail');
+  
+    img.addEventListener('dragstart', function (event) {
+      event.dataTransfer.setData('text', event.target.id);
+      console.log('dragstart');
+    });
+  
+    thumbnail.addEventListener('dragover', function (event) {
+      event.preventDefault();  // Necessary to allow dropping
+    });
+  
+    thumbnail.addEventListener('drop', function (event) {
+      event.preventDefault();
+      const data = event.dataTransfer.getData('text');
+      const draggableElement = document.getElementById(data);
+      thumbnail.appendChild(draggableElement);
+      draggableElement.style.position = 'absolute';
+      draggableElement.style.top = event.offsetY + 'px';
+      draggableElement.style.left = event.offsetX + 'px';
+    });
+  
 });
